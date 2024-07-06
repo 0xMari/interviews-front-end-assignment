b519f238bad2db370784c6309c7c20cb69dea37e
MariaJyate <maria.jyate@gmail.com>
2024-07-06 16:47:26 +0200

    route

--- recipebook-fe/src/App.js
+++ recipebook-fe/src/App.js
@@ -3,6 +3,8 @@ import Welcome from './welcome'
 import Landing from './landing'
 import RecipePage from './recipes'
 import { Route, Routes } from 'react-router-dom';
+import { RecipeDetails } from './recipe-details';
+import { AddRecipe } from './add-recipe';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path='/' element={<Welcome/>} />
           <Route path='/home' element={<Landing/>} />
           <Route path='/recipes' element={<RecipePage/>} />
+          <Route path='/recipes/:recipe' element={<RecipeDetails/>} />
+          <Route path='/recipes/add-your-recipe' element={<AddRecipe/>} />
       </Routes>
     </>
   )
